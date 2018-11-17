c39f2f304507ebad9b1b5aab75d0a9ad08c874ef
Anastasia Usoltseva <anasta.usoltseva@gmail.com>
2018-11-18 00:37:27 +0700

    Добавлены (но до конца не реализованы) сущности геометрических фигур, которые наследуются от базовой сущности. Добавлены классы настройки линии, настройки точек

---
+++ VectorEditorSolution/VectorEditorProject/Figures/Utility/LineSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorEditorProject.Figures.Utility
+{
+    class LineSettings
+    {
+        public LineSettings(Color color, float width, DashStyle style)
+        {
+            Color = color;
+            Width = width;
+            Style = style;
+        }
+
+        public Color Color { get; set; }
+        public float Width { get; set; }
+        public DashStyle Style { get; set; }
+    }
+}
---
+++ VectorEditorSolution/VectorEditorProject/Figures/Utility/PointsSettings.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorEditorProject.Figures.Utility
+{
+    class PointsSettings
+    {
+        private int _limitPoint = 0;
+        private List<Point> _points = new List<Point>();
+
+        /// <summary>
+        /// Конструктор для безлимитных фигур
+        /// </summary>
+        public PointsSettings()
+        {
+        }
+
+        /// <summary>
+        /// Конструктор для фигур с лимитным числом точек
+        /// </summary>
+        /// <param name="limitPoint"></param>
+        public PointsSettings(int limitPoint)
+        {
+            _limitPoint = limitPoint;
+        }
+
+        /// <summary>
+        /// Добавление точки
+        /// </summary>
+        /// <param name="point"></param>
+        public void AddPoint(Point point)
+        {
+            if (CanAddPoint())
+            {
+                _points.Add(point);
+            }
+        }
+
+        /// <summary>
+        /// Возможно ли добавить точку
+        /// </summary>
+        /// <returns></returns>
+        public bool CanAddPoint()
+        {
+            if (_limitPoint == 0)
+            {
+                // Если лимита нет (= 0), можно добавлять точки
+                return true;
+            }
+
+            else
+            {
+                if (_points.Count < _limitPoint)
+                {
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public IReadOnlyList<Point> GetPoints()
+        {
+            return _points;
+        }
+    }
+}
